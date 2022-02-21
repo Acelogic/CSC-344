4de7378cb338028e8907c0a879ddfa2906dd40c2
Miguel Cruz <miguelc4600@gmail.com>
2022-02-21 11:24:17 -0500

    Racket Assignment 2 Done

--- src/PA2/PA2_7.rkt
+++ src/PA2/PA2_7.rkt
@@ -3,12 +3,82 @@
 
 #lang racket
 ( require 2htdp/image )
-;; Task 6: Random
-
-(define mona (bitmap/url "https://lh3.googleusercontent.com/j-z1pzj-uBDi-URM98Ko2jw2NW62d2PSWmQafpoA3gSM0O646lzIW66efDJ6ksD5Gqq566gX91ZI5NpwbJaoj7u6cmrSa7_q4WAi8w=w289"))
-
-(define color-list (image->color-list mona))
-(define list-length(length (image->color-list mona)))
 
 
-;; Make a function that literates through the list and creates a circle for each index
+;; Task 7 Random Image (Done)
+
+( define ( random-color ) ( color ( rgb-value ) ( rgb-value ) ( rgb-value ) ) )
+( define ( rgb-value ) ( random 256 ) )
+
+
+(define (coolthing size)
+  (above
+   (beside
+    (crop/align "right" "bottom" 100 100 (circle size "solid" (random-color)))
+    (crop/align "left" "bottom" 100 100 (circle size "solid"  (random-color))))
+   (beside
+    (crop/align "right" "top" 100 100 (circle size "solid"    (random-color)))
+    (crop/align "left" "top" 100 100 (circle size "solid"     (random-color))))))
+
+
+( define ( paint-nested-triangles from to delta color1 color2 )
+   ( define side-length ( * from delta ) )
+   ( cond
+      ( ( = from to )
+        ( if ( even? from )
+             ( triangle side-length "solid" color1 )
+             ( triangle side-length "solid" color2 )
+             )
+        )
+      ( ( < from to )
+        ( if ( even? from )
+             ( overlay
+               ( triangle side-length "solid" color1 )
+               ( paint-nested-triangles ( + from 1 ) to delta color1 color2 )
+               )
+             ( overlay
+               ( triangle side-length "solid" color2 )
+               ( paint-nested-triangles ( + from 1 ) to delta color1 color2 )
+               )
+             )
+        )
+      )
+)
+
+
+;; Make a paint nested cropped circle function
+( define ( paint-nested-circle from to delta color1 color2 )
+   ( define side-length ( * from delta ) )
+   ( cond
+      ( ( = from to )
+        ( if ( even? from )
+             ( circle side-length "solid" color1 )
+             ( circle side-length "solid" color2 )
+             )
+        )
+      ( ( < from to )
+        ( if ( even? from )
+             ( overlay
+               ( circle side-length "solid" color1 )
+               ( paint-nested-circle ( + from 1 ) to delta color1 color2 )
+               )
+             ( overlay
+               ( circle side-length "solid" color2 )
+               ( paint-nested-circle ( + from 1 ) to delta color1 color2 )
+               )
+             )
+        )
+      )
+)
+
+( define (some-circle-thing side count color1 color2 )
+   ( define delta ( / side count ) )
+   (overlay
+    (coolthing 175)
+    ( paint-nested-triangles 35 count delta color2 color1 )
+    ( paint-nested-circle 1 count delta color1 color2 ))
+  
+)
+
+
+;(some-circle-thing 200 40 (random-color) (random-color))
