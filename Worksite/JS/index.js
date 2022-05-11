2c74445c23934bc70d4542aa1f372c14c3c028e8
Miguel Cruz <miguelc4600@gmail.com>
2022-05-11 05:27:29 -0400

    Updated Page Background

--- Worksite/JS/index.js
+++ Worksite/JS/index.js
@@ -8,19 +8,30 @@ $( document ).ready(function() {
 
 });
 	
+$(window).on('load',function(){
+var $win = $(window),
+    w = 0,h = 0,
+    top = [],
+    bottom = [],
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
 
-var slideIndex = 0;
-carousel();
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+$win.resize(getWidth).mousemove(function(e) {
+    // Reduced Range from 255 to 100 to make the page more easier to read
+    top = [
+        Math.round(e.pageX/w * 100),
+        Math.round(e.pageY/h * 100),
+        150
+    ];
 
+     bottom = [
+        Math.round(e.pageX/h * 100),
+        Math.round(e.pageY/w * 100),
+        150
+    ];
+   $(document.body).css('background', '-moz-linear-gradient(top, rgb('+top.join(',')+'), rgb('+bottom.join(',')+'))');
+}).resize();
 
+});
